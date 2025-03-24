f7f0b2f36ea8fe9bf3912f4e970441f20dc1736c
Sreenjoy Mallick <sreenjoymallick@Sreenjoys-MacBook-Air.local>
2025-03-25 00:22:02 +0530

    Import AuthModule to fix Google OAuth route

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -21,6 +22,7 @@ import { AppService } from './app.service';
         };
       },
     }),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
